02d7972f11f3c168fba500c9a0605c8e0e099f0c
pankaj <pankaj_046@yahoo.in>
2024-05-30 00:34:05 +0530

    added permission screen

---
+++ app/src/main/java/dev/pankaj/cleanarchitecture/presentation/permission/PermissionFragment.kt
@@ -0,0 +1,31 @@
+package dev.pankaj.cleanarchitecture.presentation.permission
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import dev.pankaj.cleanarchitecture.databinding.FragmentPermissionBinding
+
+@AndroidEntryPoint
+class PermissionFragment : Fragment() {
+
+    private var _binding: FragmentPermissionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPermissionBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
