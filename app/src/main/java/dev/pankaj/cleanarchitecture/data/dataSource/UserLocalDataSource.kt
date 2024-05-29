af6e4b1718182c1d16081794770bbc60703782f8
pankaj <pankaj_046@yahoo.in>
2024-05-30 00:24:51 +0530

    minor change

--- app/src/main/java/dev/pankaj/cleanarchitecture/data/dataSource/UserLocalDataSource.kt
+++ app/src/main/java/dev/pankaj/cleanarchitecture/data/dataSource/UserLocalDataSource.kt
@@ -1,9 +1,10 @@
 package dev.pankaj.cleanarchitecture.data.dataSource
 
 import dev.pankaj.cleanarchitecture.data.local.entity.UserEntity
+import dev.pankaj.cleanarchitecture.data.local.dao.UserDao
 
-class UserLocalDataSource(private val userDAO: UserDAO) : IUserLocalDataSource {
-    suspend fun saveUser(user: UserEntity) {
-        userDAO.save(user)
+class UserLocalDataSource(private val userDAO: UserDao) : IUserLocalDataSource {
+    override suspend fun saveUser(user: UserEntity) {
+        userDAO.insert(user)
     }
 }
